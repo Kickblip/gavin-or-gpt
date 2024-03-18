02b6afa7da5bf8cfd6415d272775e4e38805c12c
Kickblip <wyatt.zilker@gmail.com>
2024-03-18 15:59:05 -0600

    more movies

--- site/assets/snippet-list.js
+++ site/assets/snippet-list.js
@@ -6,31 +6,32 @@ const snippets = [
   },
   {
     // Sinister
-    gpt: "This is the only film from this director that is worth a damn, not amazing, but utterly solid for what it is. We get Sinister which is very creative, and sinister (lol) while being able to be genuinely spooky and engaging. That being said, the scares do wear off in multiple re watches, and as the only thing holding this film up is the unique presentation, it’s a watch once type film and then move on with your life.",
+    gpt: "This is the only film from this director that is worth a damn, not amazing, but utterly solid for what it is. We get Sinister which is very creative, and sinister (lol) while being able to be genuinely spooky and engaging. That being said, the scares do wear off in multiple re watches, and as the only thing holding this film up is the unique presentation, it's a watch once type film and then move on with your life.",
     gavin:
       "Things I liked: Cool satanic backstory stuff, Ridiculously stupid main character. Things I didn't like: Children weren't scary, Videotapes get repetitive. It was a fun horror movie but I don't think it packed enough punch for me personally.",
   },
   {
     // Barbie
-    gpt: "it's a feminist film for any young girl aspiring to be a bimbo bimbo bimbo trailer trash lifetime lesbian",
+    gpt: "it's a feminist film for any young girl aspiring to be a bimbo trailer trash lifetime lesbian",
     gavin:
-      "I love when the capitalist machine tries to make a statement on sociological affairs and fails miserably. The car chase scene made me cringe so intensely with its pristine product placement that really states my biggest problem with the film; its saying NOTHING!!! It’s moments of obnoxious meta commentary and overtly explicit statements on feminism don't contribute jack shit even when removing the obvious commercialism. In many ways I feel bad since the film was doomed to be shallow.",
+      "I love when the capitalist machine tries to make a statement on sociological affairs and fails miserably. The car chase scene made me cringe so intensely with its pristine product placement that really states my biggest problem with the film; its saying NOTHING!!!",
   },
   {
-    // PLACEHOLDER
+    // On the Count of Three
     gpt: "Anything is a Christmas classic if you put your mind to it...",
-    gavin: "",
+    gavin: "On the count of three (stars)",
   },
   {
     // The Piano Teacher
-    gpt: "One of the most painful movies about intimacy ever made that isn’t retreated into woke aesthetics. Such a strange film that is equal parts infuriating and disgusting as it is interesting and kinda relatable. Check out the bodies of top posts on Letterboxd and try to unravil behind the curtain why female viewers rate a film like this soo incredibly higher than men.",
+    gpt: "One of the most painful movies about intimacy ever made that isn't retreated into woke aesthetics. Such a strange film that is equal parts infuriating and disgusting as it is interesting and kinda relatable. Check out the bodies of top posts on Letterboxd and try to unravil behind the curtain why female viewers rate a film like this soo incredibly higher than men.",
     gavin:
-      "Though the meaning is pretty diversified to fans of the film, to me this is a film about repression and self-loathing leading to fetishizing your own insecurities, and it portrays this through the absolute masterclass performance from Huppert. Definitely not one for the family. PS: I don't know who leaked this movie to the public but all the tiktok white girls getting into this movie for all the wrong reasons pains me to see. Please stay away from Haneke you freaks!",
+      "Though the meaning is pretty diversified to fans of the film, to me this is a film about repression and self-loathing leading to fetishizing your own insecurities, and it portrays this through the absolute masterclass performance from Huppert. Definitely not one for the family.",
   },
   {
-    // TITLE
-    gpt: "A rare 5 star rating from me holy moly could not have been happier with this movie. Can’t wait to watch it again. ",
-    gavin: "",
+    // The Green Knight
+    gpt: "A rare 5 star rating from me holy moly could not have been happier with this movie. Can't wait to watch it again. ",
+    gavin:
+      "Pretty awesome movie, maybe I would have liked/disliked it more if I wasn't more familiar with the source material, because while I liked a decent portion of the new additions, I think a lot of unneeded fluff was added by the end.",
   },
   {
     // Inglourious Basterds
@@ -39,19 +40,20 @@ const snippets = [
       "This was my first Tarantino film and one I thoroughly enjoyed, I can see why people enjoy his style so much now. His long, drawn-out yet witty and delightful dialogue never feels like it panders, every small joke or little remark hints at the character's emotion without directly exposing it to us as the audience.",
   },
   {
-    // TITLE
-    gpt: "",
-    gavin: "",
+    // The Fly
+    gpt: "Pretty decent. The sequences near the end are something else.",
+    gavin: "God forbid a man do ANYTHING!",
   },
   {
-    // TITLE
-    gpt: "",
-    gavin: "",
+    // Welcome to the Dollhouse
+    gpt: "Given what I'd known, I was expecting this to be a dark comedy, but it is just a straight out dark film. A good film for sure, well written and surprising at times, with good performances to boot.",
+    gavin:
+      "This movie is odd, in some ways good and in some ways bad. The awkwardness and overall dark comedic tone hits well in a lot of scenes, feeling like a direct inspiration on the less dark yet just as awkward Napoleon Dynamite brand of comedy.",
   },
   {
-    // TITLE
-    gpt: "",
-    gavin: "",
+    // Kill Bill: Vol. 1
+    gpt: "Kill Bill is both an ode and a parody of the kung fu genre. Its ostentatiousness and its grim corners reflect a double-faced tribute to the ridicules in the genre. Quirky editing and writing on point make it a tarantino classic and the scene with the sweaty boss man is hilarious.",
+    gavin: "Lucy Liu is so fine holy fuck. The movies aight.",
   },
 ];
 
--- snippets/generate.py
+++ snippets/generate.py
@@ -5,7 +5,7 @@ completion = client.chat.completions.create(
   model="ft:gpt-3.5-turbo-0125:personal::94DTFDAW",
   messages=[
     {"role": "system", "content": "Gavin is a sarcastic and factual movie reviewer."},
-    {"role": "user", "content": "Write a review for the movie The Piano Teacher"}
+    {"role": "user", "content": "Write a review for the movie Kill Bill: Vol. 1"}
   ]
 )
 print(completion.choices[0].message.content)
